d1dfb3a3e6472bb433cc2e9e3b901d461db5f0ba
Ivan <ivan.mariic@five.agency>
2021-02-02 13:32:24 +0100

    Working on Kustomer implementation.

--- ios/KustomerSDK.m
+++ ios/KustomerSDK.m
@@ -5,4 +5,7 @@
 RCT_EXTERN_METHOD(presentSupport)
 RCT_EXTERN_METHOD(presentKnowledgeBase)
 RCT_EXTERN_METHOD(resetTracking)
+RCT_EXTERN_METHOD(identify)
+RCT_EXTERN_METHOD(describeCustomer)
+
 @end
--- ios/KustomerSDK.swift
+++ ios/KustomerSDK.swift
@@ -18,4 +18,31 @@ class KustomerSDK: NSObject {
     public func resetTracking(){
         Kustomer.logOut({ error in })
     }
+
+    @objc 
+    public func identify(hash: String){
+       Kustomer.identifyCurrentCustomer(jwt: hash, { result in
+                switch result {
+                case .success:
+                    print("Success")
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            }
+       )
+    }
+    
+
+    @objc
+    public func describeCustomer(data: Dictionary){
+
+        Kustomer.chatProvider.describeCurrentCustomer(phone:data.phone, email: data.email, custom: data.custom) { result in
+                    switch result {
+                        case .success:
+                        print("ok")
+                        case .failure(let error):
+                        print(error)
+                    }
+              }
+    }
 }
