22061450d40ed73cec7f58d52dc7aafab14d14dc
Ivan <ivan.mariic@five.agency>
2021-02-03 11:11:15 +0100

    Working on describeCustomer method.

--- android/src/main/java/co/reby/rnkustomersdk/KustomerSDKModule.kt
+++ android/src/main/java/co/reby/rnkustomersdk/KustomerSDKModule.kt
@@ -1,14 +1,16 @@
 package co.reby.rnkustomersdk;
 
-import android.app.Application
 import androidx.lifecycle.liveData
-import com.facebook.react.bridge.ReactApplicationContext;
-import com.facebook.react.bridge.ReactContextBaseJavaModule;
-import com.facebook.react.bridge.ReactMethod;
-import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.*
+import com.facebook.stetho.json.ObjectMapper
+
 
 import com.kustomer.core.models.KusResult
+import com.kustomer.core.models.chat.KusCustomerDescribeAttributes
+import com.kustomer.core.models.chat.KusEmail
+import com.kustomer.core.models.chat.KusPhone
 import com.kustomer.ui.Kustomer;
+import org.json.JSONObject
 
 class KustomerSDKModule(reactContext:ReactApplicationContext):ReactContextBaseJavaModule(reactContext) {
     private val reactContext:ReactApplicationContext
@@ -32,14 +34,36 @@ class KustomerSDKModule(reactContext:ReactApplicationContext):ReactContextBaseJa
     }
 
     @ReactMethod
+    suspend fun describeCustomer(data: ReadableMap){
+
+        val email = data.getString("email")
+        val phone = data.getString("phone")
+        val custom = data.getMap("custom")
+        val convertedCustom = custom?.let { convertMapToJson(it) }
+        val objectMapper = ObjectMapper()
+
+
+        val attributes = KusCustomerDescribeAttributes(
+                emails = listOf(KusEmail(email!!)),
+                phones = listOf(KusPhone(phone!!)),
+                custom = objectMapper.convertValue(convertedCustom!!, HashMap<String, String>().javaClass)
+        )
+
+      Kustomer.getInstance().describeCustomer(attributes)
+
+    }
+    
+    @ReactMethod
     fun presentSupport(apiKey: String) {
         Kustomer.getInstance().openNewConversation()
     }
+
     @ReactMethod
     fun presentKnowledgeBase() {
         val activity = getCurrentActivity()
         //Kustomer.presentKnowledgeBase(activity);
     }
+
     @ReactMethod
     fun openConversationsCount(promise:Promise) {
         val activeConversationIds = liveData {
@@ -47,13 +71,36 @@ class KustomerSDKModule(reactContext:ReactApplicationContext):ReactContextBaseJa
         }
         promise.resolve(activeConversationIds)
     }
+
     @ReactMethod
     fun resetTracking() {
         Kustomer.getInstance().logOut()
     }
+
     @ReactMethod
     fun setCurrentPageName(screen:String) {
         //Kustomer.setCurrentPageName(screen);
+    }
+
+    private fun convertMapToJson(readableMap:ReadableMap):JSONObject {
+        val jsonObject = JSONObject()
+        val iterator = readableMap.keySetIterator()
+        while (iterator.hasNextKey())
+        {
+            val key = iterator.nextKey()
+            when (readableMap.getType(key)) {
+                ReadableType.Boolean -> jsonObject.put(key, readableMap.getBoolean(key))
+                ReadableType.Number -> jsonObject.put(key, readableMap.getDouble(key))
+                ReadableType.String -> {
+                    val value = readableMap.getString(key)
+                    if (value != null && !value.isEmpty())
+                    {
+                        jsonObject.put(key, value)
+                    }
+                }
+            }
+        }
+        return jsonObject
     }
 
 }
